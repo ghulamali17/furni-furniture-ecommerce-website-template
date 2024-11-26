c603a479bda4e5a530e009248377ded35bd0c9bc
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-11-26 16:47:01 +0500

    testimonial section completed

---
+++ main.js
@@ -0,0 +1,16 @@
+const tnsslider = tns({
+    container: ".slider-wrapper", 
+    items: 1,                    
+    slideBy: 1,
+    speed: 700,
+    nav: true,                     
+    navPosition: "bottom",        
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayButtonOutput: false, 
+    controls: true,
+    controlsContainer: "#controls",
+    prevButton: '.prev',
+    nextButton: '.next'
+  });
+  
