010aaa8b0e33517ab76b1556564616709d94cf5a
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-11-28 09:18:00 +0500

    Responsive Navbar

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const tnsslider = tns({
-    container: ".slider-wrapper", 
-    items: 1,                    
+function toggleMenu() {
+  const menuList = document.getElementById("nav_links");
+  if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+    menuList.style.maxHeight = "600px";
+  } else {
+    menuList.style.maxHeight = "0px";
+  }
+}
+
+const removeActiveClassOnResize = () => {
+  const navLinks = document.querySelectorAll(".nav_links a");
+  if (window.innerWidth < 768) {
+    navLinks.forEach((link) => link.classList.remove("active"));
+  }
+};
+window.addEventListener("resize", removeActiveClassOnResize);
+removeActiveClassOnResize();
+
+if (document.querySelector(".slider-wrapper")) {
+  const tnsslider = tns({
+    container: ".slider-wrapper",
+    items: 1,
     slideBy: 1,
     speed: 700,
-    nav: true,                     
-    navPosition: "bottom",        
+    nav: true,
+    navPosition: "bottom",
     autoplay: true,
     autoplayTimeout: 4000,
-    autoplayButtonOutput: false, 
+    autoplayButtonOutput: false,
     controls: true,
     controlsContainer: "#controls",
-    prevButton: '.prev',
-    nextButton: '.next'
+    prevButton: ".prev",
+    nextButton: ".next",
   });
-  
+}
